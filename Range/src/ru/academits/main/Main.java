9167d39a30d49228ec4d5d440e756fd11818034e
Kirill <dyky.ks@gmail.com>
2022-07-14 15:31:16 +0300

    change structure

--- Range/src/ru/academits/main/Main.java
+++ Range/src/ru/academits/main/Main.java
@@ -1,3 +1,5 @@
+package ru.academits.main;
+
 public class Main {
     public static void main(String[] args) {
 
--- Range/src/ru/academits/range/Range.java
+++ Range/src/ru/academits/range/Range.java
@@ -1,3 +1,5 @@
+package ru.academits.range;
+
 public class Range {
     private double from;
     private double to;

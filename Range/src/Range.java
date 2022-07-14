4467aae5e99e23611d5c6fa3e3a40d6cdfab7b79
ReygarTarg <kirill.shash@mail.ru>
2022-07-14 15:10:30 +0300

    Add constructor, getter and setter

---
+++ Range/src/Range.java
@@ -0,0 +1,33 @@
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+
+    public int getLength(){
+        return 1;
+    }
+
+    public boolean isInside(double number){
+        return true;
+    }
+}
